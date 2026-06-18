4e3b38a02f64d5b194c46a2af951df8148ac14e2
Donald A. Cupp Jr. <doncuppjr@yahoo.com>
2026-06-18 13:06:25 -0500

    modified:   packaging/nfpm.yaml 	modified:   packaging/scripts/postinstall.sh 	modified:   packaging/systemd/lego-update.service

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+useradd --system \
+        --home /etc/lego \
+        --shell /usr/sbin/nologin \
+        lego
+
+chown lego:lego /etc/lego
+
 systemctl daemon-reload || true
 systemctl enable lego-update.timer || true
 
