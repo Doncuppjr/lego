e30db9f60d366e11ddba4a174a68425bdac5d051
Donald A. Cupp Jr. <doncuppjr@yahoo.com>
2026-06-18 12:14:21 -0600

    modified:   packaging/bin/lego-init 	new file:   packaging/examples/hooks/deploy.d/50-export-mcs-jks 	new file:   packaging/examples/hooks/post.d/90-start-webservers 	new file:   packaging/examples/hooks/pre.d/10-stop-webservers 	modified:   packaging/lego/lego.yml.example 	new file:   packaging/lib/lego/hooks/run-deploy-hooks 	new file:   packaging/lib/lego/hooks/run-post-hooks 	new file:   packaging/lib/lego/hooks/run-pre-hooks 	modified:   packaging/nfpm.yaml 	modified:   packaging/scripts/postinstall.sh 	modified:   packaging/systemd/lego-update.service

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-useradd --system \
-        --home /etc/lego \
-        --shell /usr/sbin/nologin \
-        lego
-
-chown lego:lego /etc/lego
+install -d -m 0755 /etc/lego /etc/lego/hooks /etc/lego/hooks/pre.d /etc/lego/hooks/deploy.d /etc/lego/hooks/post.d
+install -d -m 0700 /var/lib/lego
 
 systemctl daemon-reload || true
-systemctl enable lego-update.timer || true
+systemctl enable --now lego-update.timer || true
 
 echo
 echo "lego installed."
 echo "To create /etc/lego/lego.yml, run:"
 echo "  sudo lego-init"
+echo
+echo "Optional hook examples are installed under:"
+echo "  /usr/share/doc/lego/examples/hooks"
 echo
